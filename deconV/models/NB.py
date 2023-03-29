81079e76de60634e37b1813564f76c0fc07e9d38
lutrarutra <artur.gynter@hotmail.com>
2023-03-29 14:33:31 +0300

    fixed nb model

--- deconV/models/NB.py
+++ deconV/models/NB.py
@@ -20,7 +20,8 @@ class NB(Base):
     def __init__(self, adata, labels_key, dropout_type: Literal["separate", "shared", None] = "separate", device="cpu"):
         super().__init__(adata, labels_key, dropout_type, device)
 
-        self.log_total_count0 = 5 * torch.ones(self.n_genes, self.n_labels, device=self.device)
+        self.mu_total_count0 = 5 * torch.ones((self.n_labels, self.n_genes), device=self.device)
+        self.std_total_count0 = 1 * torch.ones((self.n_labels, self.n_genes), device=self.device)
 
         self.alpha0 = 1.0 * torch.ones(self.n_genes, device=self.device)
         self.beta0 = 50.0 * torch.ones(self.n_genes, device=self.device)
@@ -55,30 +56,40 @@ class NB(Base):
             dist.constraints.positive
         )
 
-        log_total_count = pyro.param(
-            "log_total_count",
-            self.log_total_count0,
+        mu_total_count = pyro.param(
+            "mu_total_count",
+            self.mu_total_count0,
             constraint=dist.constraints.real
+        )
+
+        std_total_count = pyro.param(
+            "std_total_count",
+            self.std_total_count0,
+            constraint=dist.constraints.positive
         )
 
         with pyro.plate("genes", self.n_genes, device=self.device):
             probs = pyro.sample("probs", dist.Beta(alpha, beta))
-            
-        with pyro.plate("obs", len(labels), device=self.device), poutine.scale(scale=1.0/len(labels)):
-            total_count = log_total_count.exp()[:, labels].T
-            if self.ref_dropout_type is not None:
-                obs_dist = dist.ZeroInflatedNegativeBinomial(
-                    total_count=total_count,
-                    probs=probs,
-                    gate_logits=dropout_logits,
-                ).to_event(1)
-            else:
-                obs_dist = dist.NegativeBinomial(
-                    total_count=total_count,
-                    probs=probs,
-                ).to_event(1)
+            with pyro.plate("labels", self.n_labels, device=self.device):
+                total_count = pyro.sample("total_count", dist.LogNormal(mu_total_count, std_total_count))
 
-            pyro.sample("sc_obs", obs_dist, obs=sc_counts)
+            
+            with pyro.plate("cells", len(labels), device=self.device), poutine.scale(scale=1.0/len(labels)):
+                total_count = total_count[labels,:]
+
+                if self.ref_dropout_type is not None:
+                    obs_dist = dist.ZeroInflatedNegativeBinomial(
+                        total_count=total_count,
+                        probs=probs,
+                        gate_logits=dropout_logits,
+                    )
+                else:
+                    obs_dist = dist.NegativeBinomial(
+                        total_count=total_count,
+                        probs=probs,
+                    )
+
+                pyro.sample("sc_obs", obs_dist, obs=sc_counts)
     
         
     def ref_guide(self, sc_counts, labels):
@@ -92,8 +103,25 @@ class NB(Base):
             dist.constraints.positive
         )
 
+        mu_total_count = pyro.param(
+            "mu_total_count",
+            self.mu_total_count0,
+            constraint=dist.constraints.real
+        )
+
+        std_total_count = pyro.param(
+            "std_total_count",
+            self.std_total_count0,
+            constraint=dist.constraints.positive
+        )
+
         with pyro.plate("genes", self.n_genes, device=self.device):
             pyro.sample("probs", dist.Beta(alpha, beta))
+            with pyro.plate("labels", self.n_labels, device=self.device):
+                pyro.sample("total_count", dist.LogNormal(mu_total_count, std_total_count))
+
+        # with pyro.plate("genes", self.n_genes, device=self.device):
+        #     pyro.sample("probs", dist.Beta(alpha, beta))
 
     def dec_model(self, bulk):
         n_samples = len(bulk)
@@ -109,16 +137,19 @@ class NB(Base):
         alpha = self.params["alpha"]
         beta = self.params["beta"]
 
-        ct_total_count = self.params["log_total_count"].exp()
-        
+        mu_total_count = self.params["mu_total_count"]
+        std_total_count = self.params["std_total_count"]
+
         with pyro.plate("genes", self.n_genes, device=self.device):
             probs = pyro.sample("probs", dist.Beta(alpha, beta))
+            with pyro.plate("labels", self.n_labels, device=self.device):
+                ct_total_count = pyro.sample("total_count", dist.LogNormal(mu_total_count, std_total_count))
+    
 
         with pyro.plate("samples", n_samples, device=self.device):
             proportions = pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
-
-            total_count = torch.sum(proportions.unsqueeze(0) * ct_total_count.unsqueeze(1), dim=-1)
-            total_count = log_cell_counts.exp() * total_count
+            total_count = torch.sum(proportions.unsqueeze(-1) * ct_total_count.unsqueeze(0), dim=1)
+            total_count = log_cell_counts.exp() * total_count.T
 
             if self.dec_model_dropout:
                 dropout = logits2probs(self.params["dropout_logits"])
@@ -153,9 +184,17 @@ class NB(Base):
 
         alpha = self.params["alpha"]
         beta = self.params["beta"]
-        
+
+        mu_total_count = self.params["mu_total_count"]
+        std_total_count = self.params["std_total_count"]
+
         with pyro.plate("genes", self.n_genes, device=self.device):
             pyro.sample("probs", dist.Beta(alpha, beta))
+            with pyro.plate("labels", self.n_labels, device=self.device):
+                pyro.sample("total_count", dist.LogNormal(mu_total_count, std_total_count))
+        
+        # with pyro.plate("genes", self.n_genes, device=self.device):
+        #     pyro.sample("probs", dist.Beta(alpha, beta))
 
     def pseudo_bulk(self, n_samples=1000):
         if self.log_concentrations is None:
@@ -196,9 +235,11 @@ class NB(Base):
 
         a = self.params["alpha"][gene_i]
         b = self.params["beta"][gene_i]
+        mu_total_count = self.params["mu_total_count"][ct_i, gene_i]
+        std_total_count = self.params["std_total_count"][ct_i, gene_i]
         probs = dist.Beta(a, b).sample((n_samples,))
 
-        total_count = self.params["log_total_count"][gene_i, ct_i].exp()
+        total_count = dist.LogNormal(mu_total_count, std_total_count).sample((n_samples,))
 
         if self.ref_dropout_type is not None:
             if self.ref_dropout_type == "separate":
