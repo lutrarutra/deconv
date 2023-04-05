fixed numerical issue

deconV/DeconV.py
@@ -86,7 +86,7 @@ class DeconV:
         return proportions
     
     def get_results_df(self, quantiles=(0.025, 0.975)):
-        if self.deconvolution_module.log_concentrations is None:
+        if self.deconvolution_module.concentrations is None:
             raise ValueError("Please run deconvolute() first.")
         
         limits = np.empty((self.n_bulk_samples, self.n_labels, 2))
@@ -96,7 +96,7 @@ class DeconV:
             proportions = self.sum_sub_proportions(proportions).cpu()
         else:
             for i in range(self.n_bulk_samples):
-                p_dist = dist.Dirichlet(self.deconvolution_module.log_concentrations[i].exp())
+                p_dist = dist.Dirichlet(self.deconvolution_module.concentrations[i])
                 ps = p_dist.sample((10000,)).cpu()
                 q = np.quantile(ps, quantiles, 0)
                 limits[i, :, :] = q.T

deconV/models/Gamma.py
@@ -90,11 +90,15 @@ class Gamma(Base):
     def dec_model(self, bulk):
         n_samples = len(bulk)
 
-        log_concentration = torch.zeros((n_samples, self.n_labels), device=self.device)
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
+            constraint=dist.constraints.positive
+        )
 
-        log_cell_counts = pyro.param(
-            "log_cell_counts",
-            7.0 * torch.ones(n_samples, device=self.device),
+        cell_counts = pyro.param(
+            "cell_counts",
+            1e7 * torch.ones(n_samples, device=self.device),
             constraint=dist.constraints.positive
         )
 
@@ -106,10 +110,10 @@ class Gamma(Base):
                 theta = pyro.sample("theta", dist.Gamma(alpha, beta))
 
         with pyro.plate("samples", n_samples, device=self.device):
-            proportions = pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            proportions = pyro.sample("proportions", dist.Dirichlet(concentrations))
 
             rate = torch.sum(proportions.unsqueeze(0) * theta.unsqueeze(1), dim=-1)
-            rate = log_cell_counts.exp() * rate
+            rate = cell_counts * rate
 
             if self.dec_model_dropout:
                 dropout = logits2probs(self.params["dropout_logits"])
@@ -126,14 +130,14 @@ class Gamma(Base):
     def dec_guide(self, bulk):
         n_samples = len(bulk)
         
-        log_concentration = pyro.param(
-            "log_concentration",
-            torch.zeros((n_samples, self.n_labels), device=self.device),
-            constraint=dist.constraints.real
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device),
+            constraint=dist.constraints.positive
         )
 
         with pyro.plate("samples", n_samples, device=self.device):
-            pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            pyro.sample("proportions", dist.Dirichlet(concentrations))
 
         alpha = self.params["alpha"]
         beta = self.params["beta"]
@@ -144,7 +148,7 @@ class Gamma(Base):
 
 
     def pseudo_bulk(self):
-        if self.log_concentrations is None:
+        if self.concentrations is None:
             raise ValueError("Run deconvolute() first")
         alpha = self.params["alpha"]
         beta = self.params["beta"]

deconV/models/LogNormal.py
@@ -91,11 +91,15 @@ class LogNormal(Base):
     def dec_model(self, bulk):
         n_samples = len(bulk)
 
-        log_concentration = torch.zeros((n_samples, self.n_labels), device=self.device)
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
+            constraint=dist.constraints.positive
+        )
 
-        log_cell_counts = pyro.param(
-            "log_cell_counts",
-            7.0 * torch.ones(n_samples, device=self.device),
+        cell_counts = pyro.param(
+            "cell_counts",
+            1e7 * torch.ones(n_samples, device=self.device),
             constraint=dist.constraints.positive
         )
 
@@ -107,10 +111,10 @@ class LogNormal(Base):
                 theta = pyro.sample("theta", dist.LogNormal(mu, sigma))
 
         with pyro.plate("samples", n_samples, device=self.device):
-            proportions = pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            proportions = pyro.sample("proportions", dist.Dirichlet(concentrations))
 
             rate = torch.sum(proportions.unsqueeze(0) * theta.unsqueeze(1), dim=-1)
-            rate = log_cell_counts.exp() * rate
+            rate = cell_counts * rate
 
             if self.dec_model_dropout:
                 dropout = logits2probs(self.params["dropout_logits"])
@@ -128,14 +132,14 @@ class LogNormal(Base):
     def dec_guide(self, bulk):
         n_samples = len(bulk)
         
-        log_concentration = pyro.param(
-            "log_concentration",
-            torch.zeros((n_samples, self.n_labels), device=self.device),
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
             constraint=dist.constraints.real
         )
 
         with pyro.plate("samples", n_samples, device=self.device):
-            pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            pyro.sample("proportions", dist.Dirichlet(concentrations))
 
         mu = self.params["mu"]
         sigma = self.params["sigma"]
@@ -145,7 +149,7 @@ class LogNormal(Base):
                 pyro.sample("theta", dist.LogNormal(mu, sigma))
 
     def pseudo_bulk(self):
-        if self.log_concentrations is None:
+        if self.concentrations is None:
             raise ValueError("Run deconvolute() first")
         
         mu = self.params["mu"]

deconV/models/NB.py
@@ -126,11 +126,15 @@ class NB(Base):
     def dec_model(self, bulk):
         n_samples = len(bulk)
 
-        log_concentration = torch.zeros((n_samples, self.n_labels), device=self.device)
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
+            constraint=dist.constraints.positive
+        )
 
-        log_cell_counts = pyro.param(
-            "log_cell_counts",
-            7.0 * torch.ones(n_samples, device=self.device),
+        cell_counts = pyro.param(
+            "cell_counts",
+            1e7 * torch.ones(n_samples, device=self.device),
             constraint=dist.constraints.positive
         )
 
@@ -147,9 +151,9 @@ class NB(Base):
     
 
         with pyro.plate("samples", n_samples, device=self.device):
-            proportions = pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            proportions = pyro.sample("proportions", dist.Dirichlet(concentrations))
             total_count = torch.sum(proportions.unsqueeze(-1) * ct_total_count.unsqueeze(0), dim=1)
-            total_count = log_cell_counts.exp() * total_count.T
+            total_count = cell_counts * total_count.T
 
             if self.dec_model_dropout:
                 dropout = logits2probs(self.params["dropout_logits"])
@@ -173,14 +177,14 @@ class NB(Base):
     def dec_guide(self, bulk):
         n_samples = len(bulk)
         
-        log_concentration = pyro.param(
-            "log_concentration",
-            torch.zeros((n_samples, self.n_labels), device=self.device),
-            constraint=dist.constraints.real
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
+            constraint=dist.constraints.positive
         )
 
         with pyro.plate("samples", n_samples, device=self.device):
-            pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            pyro.sample("proportions", dist.Dirichlet(concentrations))
 
         alpha = self.params["alpha"]
         beta = self.params["beta"]
@@ -197,7 +201,7 @@ class NB(Base):
         #     pyro.sample("probs", dist.Beta(alpha, beta))
 
     def pseudo_bulk(self):
-        if self.log_concentrations is None:
+        if self.concentrations is None:
             raise ValueError("Run deconvolute() first")
         
         alpha = self.params["alpha"]

deconV/models/Static.py
@@ -69,21 +69,25 @@ class Static(Base):
     def dec_model(self, bulk):
         n_samples = len(bulk)
 
-        log_concentration = torch.zeros((n_samples, self.n_labels), device=self.device)
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
+            constraint=dist.constraints.positive
+        )
 
-        log_cell_counts = pyro.param(
-            "log_cell_counts",
-            7.0 * torch.ones(n_samples, device=self.device),
+        cell_counts = pyro.param(
+            "cell_counts",
+            1e7 * torch.ones(n_samples, device=self.device),
             constraint=dist.constraints.positive
         )
 
         rate = self.params["log_rate"].exp()
 
         with pyro.plate("samples", n_samples, device=self.device):
-            proportions = pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            proportions = pyro.sample("proportions", dist.Dirichlet(concentrations))
 
             rate = torch.sum(proportions.unsqueeze(0) * rate.unsqueeze(1), dim=-1)
-            rate = log_cell_counts.exp() * rate
+            rate = cell_counts * rate
 
             if self.dec_model_dropout:
                 dropout = logits2probs(self.params["dropout_logits"])
@@ -100,17 +104,17 @@ class Static(Base):
     def dec_guide(self, bulk):
         n_samples = len(bulk)
         
-        log_concentration = pyro.param(
-            "log_concentration",
-            torch.zeros((n_samples, self.n_labels), device=self.device),
-            constraint=dist.constraints.real
+        concentrations = pyro.param(
+            "concentrations",
+            torch.ones((n_samples, self.n_labels), device=self.device, dtype=torch.float64),
+            constraint=dist.constraints.positive
         )
 
         with pyro.plate("samples", n_samples, device=self.device):
-            pyro.sample("proportions", dist.Dirichlet(log_concentration.exp()))
+            pyro.sample("proportions", dist.Dirichlet(concentrations))
 
     def pseudo_bulk(self):
-        if self.log_concentrations is None:
+        if self.concentrations is None:
             raise ValueError("Run deconvolute() first")
         
         theta = self.params["log_rate"].exp()

deconV/models/base.py
@@ -50,7 +50,7 @@ class Base(ABC):
         self.n_labels = len(adata.obs[labels_key].cat.categories)
         
         self.params = None
-        self.log_concentrations = None
+        self.concentrations = None
 
         self.ref_dropout_type = dropout_type
         self.device = device
@@ -104,7 +104,7 @@ class Base(ABC):
         assert bulk.shape[1] == self.n_genes, "The bulk data must have the same number of genes as the reference data"
         
         def get_optim_params(param_name):
-            if param_name == "log_cell_counts":
+            if param_name == "cell_counts":
                 return dict(lr=lr, lrd=lrd)
             else:
                 return dict(lr=lr, lrd=lrd)
@@ -126,18 +126,18 @@ class Base(ABC):
                     lr=f"{list(optim.get_state().values())[0]['param_groups'][0]['lr']:.2e}",
                 )
             
-        self.log_concentrations = pyro.param("log_concentration").clone().detach()
-        self.log_cell_counts = pyro.param("log_cell_counts").clone().detach()
+        self.concentrations = pyro.param("concentrations").clone().detach()
+        self.cell_counts = pyro.param("cell_counts").clone().detach()
 
     def get_proportions(self):
-        if self.log_concentrations is None:
+        if self.concentrations is None:
             raise ValueError("You must deconvolute first")
-        return dist.Dirichlet(self.log_concentrations.exp()).mean 
+        return dist.Dirichlet(self.concentrations).mean 
 
     def get_cell_counts(self):
-        if self.log_cell_counts is None:
+        if self.cell_counts is None:
             raise ValueError("You must deconvolute first")
-        return self.log_cell_counts.exp() 
+        return self.cell_counts
 
     @abstractmethod
     def ref_model(self, sc_counts, labels):
@@ -164,14 +164,14 @@ class Base(ABC):
         pass
 
     def save_model(self, dir):
-        if self.log_concentrations is None:
+        if self.concentrations is None:
             raise ValueError("You must deconvolute first")
 
         if not os.path.exists(dir):
             os.makedirs(dir)
         
-        with open(os.path.join(dir, "log_concentrations.npy"), "wb") as f:
-            np.save(f, self.log_concentrations.cpu().numpy())
+        with open(os.path.join(dir, "concentrations.npy"), "wb") as f:
+            np.save(f, self.concentrations.cpu().numpy())
 
         with open(os.path.join(dir, "ref.pkl"), "wb") as f:
             params = self.params
@@ -184,8 +184,8 @@ class Base(ABC):
 
     # @classmethod
     # def load_model(cls, dir, adata, labels_key):
-    #     with open(os.path.join(dir, "log_concentrations.npy"), "rb") as f:
-    #         log_concentrations = torch.tensor(np.load(f))
+    #     with open(os.path.join(dir, "concentrations.npy"), "rb") as f:
+    #         concentrations = torch.tensor(np.load(f))
 
 
     #     with open(os.path.join(dir, "ref.pkl"), "rb") as f:
@@ -214,7 +214,7 @@ class Base(ABC):
     #         model = Static(adata, labels_key, ref_dropout_type)
 
         
-    #     model.log_concentrations = log_concentrations
+    #     model.concentrations = concentrations
     #     model.params = params
     #     model.dec_model_dropout = dec_model_dropout
 
@@ -222,14 +222,14 @@ class Base(ABC):
 
 
     def res_df(self):
-        assert self.log_concentrations is not None, "You must deconvolute first"
-        n_bulk_samples = self.log_concentrations.shape[0]
+        assert self.concentrations is not None, "You must deconvolute first"
+        n_bulk_samples = self.concentrations.shape[0]
         quantiles = np.empty((n_bulk_samples, self.n_labels, 2))
 
-        res = dist.Dirichlet(self.log_concentrations.exp()).mean
+        res = dist.Dirichlet(self.concentrations).mean
 
         for i in range(n_bulk_samples):
-            p_dist = dist.Dirichlet(self.log_concentrations[i].exp())
+            p_dist = dist.Dirichlet(self.concentrations[i])
             ps = p_dist.sample((10000,))
             q = np.quantile(ps, (0.025, 0.975), 0)
             quantiles[i, :, :] = q.T