ef99b539de403c39930a4c7967c6fa7c1071bb89
lutrarutra <artur.gynter@hotmail.com>
2023-05-25 15:18:22 +0300

    scaden normalized

--- benchmarks/3rd-party/scaden/pbmc/run.sh
+++ benchmarks/3rd-party/scaden/pbmc/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd data
-scaden simulate --cells 100 --n_samples 32000 --pattern "*_counts.txt"
+scaden simulate --n_samples 32000 --pattern "*_counts.txt"
 scaden process data.h5ad pbmc_bulk_data.txt
 scaden train processed.h5ad --steps 5000 --model_dir model
 scaden predict --model_dir model pbmc_bulk_data.txt
